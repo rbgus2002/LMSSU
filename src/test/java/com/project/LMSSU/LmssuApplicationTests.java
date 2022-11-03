e4dbeeb03659e438aea5e5fc25cee286970b7d37
choegyuhyeon <rbgus2002@naver,.com>
2022-11-03 16:59:26 +0900

    프로젝트 생성

---
+++ src/test/java/com/project/LMSSU/LmssuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LMSSU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmssuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
