1bcaef99b0bcfb20ad7ce6046da2f69f64ae1f1c
choegyuhyeon <rbgus2002@naver,.com>
2022-11-03 17:06:44 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/LMSSU/LmssuApplication.java
@@ -0,0 +1,13 @@
+package com.project.LMSSU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmssuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmssuApplication.class, args);
+	}
+
+}
