ec832d1334fe700debad4e0fff5c69d888c8d3da
2seung5 <dltmdgh0904@gamil.com>
2022-11-07 19:36:28 +0900

    이승호

--- FE/index.js
+++ FE/index.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/logout', (req, res) => {
+    res.render('logout');
+});
+
 app.get('/test', function(요청, 응답) {
   응답.sendFile(__dirname + '/index.html');
 })
