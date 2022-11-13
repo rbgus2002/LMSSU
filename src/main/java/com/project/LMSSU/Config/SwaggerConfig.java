549cb66e12c7a2b711340b0d7f43f1f3f5cb5b78
choegyuhyeon <rbgus2002@naver,.com>
2022-11-13 18:48:32 +0900

    Swagger 연결

---
+++ src/main/java/com/project/LMSSU/Config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.project.LMSSU.Config;
+
+import com.project.LMSSU.DTO.LMSCrawlingRequestDTO;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@OpenAPIDefinition(
+        info = @Info(title = "LMSSU API 명세서",
+                description = "LMS 연동 학업관리 서비스 API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+}
--- src/main/java/com/project/LMSSU/Controller/TestController.java
+++ src/main/java/com/project/LMSSU/Controller/TestController.java
@@ -4,6 +4,12 @@ import com.project.LMSSU.DTO.LMSCrawlingRequestDTO;
 import com.project.LMSSU.Service.LMSCrawlingService;
 import com.project.LMSSU.Service.StudentService;
 import com.project.LMSSU.Service.TestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.Map;
 
+@Tag(name = "TestController", description = "테스트용 컨트롤러")
 @RequiredArgsConstructor
 @RestController
 public class TestController {
+
+
     private final TestService testService;
     private final LMSCrawlingService lmsCrawlingService;
 
+    @Operation(summary = "Test 메서드", description = "Test 메서드입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @GetMapping("/")
     public String test() {
         return "asdasdsadsa";
