e400cd010cf7b61f403bea832cfeb3a5f8222d0d
choegyuhyeon <rbgus2002@naver,.com>
2022-12-05 18:11:59 +0900

    Linux LocalDateTime Test

--- src/main/java/com/project/LMSSU/Controller/TestController.java
+++ src/main/java/com/project/LMSSU/Controller/TestController.java
@@ -1,6 +1,7 @@
 package com.project.LMSSU.Controller;
 
 import com.project.LMSSU.DTO.StudentLoginRequestDTO;
+import com.project.LMSSU.Repository.SubjectRepository;
 import com.project.LMSSU.Service.TestService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -20,6 +24,7 @@ public class TestController {
 
 
     private final TestService testService;
+    private final SubjectRepository  subjectRepository;
 
 
     @Operation(summary = "Test 메서드", description = "Test 메서드입니다.")
@@ -33,8 +38,19 @@ public class TestController {
     public String test() {
         return "asdasdsadsa";
     }
+
     @GetMapping("/weeks")
     public void addWeeks() {
         testService.addWeeks();
+    }
+
+    @GetMapping("/time")
+    public Map testTime() {
+        Map map = new HashMap();
+        map.put("currentTime", LocalDateTime.now());
+        map.put("opensourceUpdateTime", subjectRepository.findById(2150061301L).get().getUpdateTime());
+        map.put("differenceTime", ChronoUnit.HOURS.between(subjectRepository.findById(2150061301L).get().getUpdateTime(), LocalDateTime.now()));
+
+        return map;
     }
 }
